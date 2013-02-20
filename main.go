aaae13682a95aa90599ad79676ba3ce55df0483b
James Pirruccello <james@parchment.com>
2013-02-19 19:59:01 -0500

    Barebones Entry type created and a sample code creating a slice of 10 Entries is created.

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
     "fmt"
+    "time"
+    "github.com/carbocation/forum.git/forum"
 )
 
 func main() {
-    fmt.Println("Hello, world!")
+    // Make 10 entries based on a skeleton; the Id's will be appropriately distinct.
+    N := 10
+    skeleton := forum.Entry{ Id: 1, Title: "Hello, world", Body: "This is a body.", Created: time.Now(), AuthorId: 1}
+    var entries = make([]forum.Entry,N)
+    for i := 0; i < N; i++ {
+        entries[i], entries[i].Id, entries[i].Created = skeleton, int64(i+1), time.Now()
+    }
+
+    fmt.Printf("Created new entries with the following data:\n%#v\n",entries)
 }
