d4d08671726a02f0dd179a1eedea8fef6fa7a556
James Pirruccello <james@carbocation.com>
2013-04-09 16:31:00 -0400

    Making Delta more rational.

--- forum/delta.go
+++ forum/delta.go
@@ -5,7 +5,8 @@ import (
 )
 
 type Delta struct {
-	Id         int64     "ID of the modified post"
+	Id         int64     "Unique identifier of this delta"
+	PostId     int64     "ID of the modified post"
 	TitleDelta string    "Changes made to the title, if any"
 	BodyDelta  string    "Changes made to the body, if any"
 	Modified   time.Time "Time at which the changes were made"
