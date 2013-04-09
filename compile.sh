e7cd13e3477745d474e10b8013ab2392306e584a
James Pirruccello <james@carbocation.com>
2013-04-09 12:04:15 -0400

    Incorporated nrsc into main build scripts.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o forum.linux  *.go
-GOOS=darwin GOARCH=amd64 go build -o forum.osx  *.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o forum.linux  *.go && ./nrsc-script forum.linux static
+GOOS=darwin GOARCH=amd64 go build -o forum.osx  *.go && ./nrsc-script forum.osx static
 
