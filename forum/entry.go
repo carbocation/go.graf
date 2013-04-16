f990f5beb9ca7f648f377164e1f3bf54a37e618a
James Pirruccello <james@carbocation.com>
2013-04-16 14:07:01 -0400

    Renamed sql.go to queries.go. Moved the functions into the same file as the entries type, since these basically apply to entries. Now, the queries file holds onto all query text, so the queries themselves can be found in one place.

--- forum/entry.go
+++ forum/entry.go
@@ -2,6 +2,8 @@ package forum
 
 import (
 	"time"
+
+	"github.com/carbocation/util.git/datatypes/closuretable"
 )
 
 // Put ModifiedBy, ModifiedAuthor in a separate table. A post can only be 
@@ -23,4 +25,77 @@ type EntryView struct {
 	Entry
 	Points   int64
 	HasVoted bool
+}
+
+// Retrieves all entries that are descendants of the ancestral entry, including the ancestral entry itself
+func DescendantEntries(root int64) (entries map[int64]Entry, err error) {
+	stmt, err := Config.DB.Prepare(queries.DescendantEntries)
+	defer stmt.Close()
+	if err != nil {
+		return
+	}
+
+	// Query from that prepared statement
+	rows, err := stmt.Query(root)
+	if err != nil {
+		return
+	}
+
+	entries = map[int64]Entry{}
+
+	var id, authorid int64
+	var title, body string
+	var created time.Time
+
+	// Iterate over the rows
+	for rows.Next() {
+		err = rows.Scan(&id, &title, &body, &created, &authorid)
+		if err != nil {
+			return
+		}
+
+		entries[id] = Entry{Id: id, Title: title, Body: body, Created: created, AuthorId: authorid}
+	}
+
+	return
+}
+
+//Returns a closure table of IDs that are descendants of a given ID
+func ClosureTable(id int64) (ct *closuretable.ClosureTable, err error) {
+	ct = closuretable.New(id)
+
+	// Pull down the remaining elements in the closure table that are descendants of this node
+	stmt, err := Config.DB.Prepare(queries.DescendantClosureTable)
+	if err != nil {
+		//fmt.Printf("Statement Preparation Error: %s", err)
+		return
+	}
+
+	rows, err := stmt.Query(id)
+	defer stmt.Close()
+	if err != nil {
+		//fmt.Printf("Query Error: %v", err)
+		return
+	}
+
+	//Populate the closuretable
+	for rows.Next() {
+		var ancestor, descendant int64
+		var depth int
+		err = rows.Scan(&ancestor, &descendant, &depth)
+		if err != nil {
+			//fmt.Printf("Rowscan error: %s", err)
+			return
+		}
+
+		err = ct.AddChild(closuretable.Child{Parent: ancestor, Child: descendant})
+
+		//err = ct.AddRelationship(closuretable.Relationship{Ancestor: ancestor, Descendant: descendant, Depth: depth})
+		if err != nil {
+			//fmt.Fprintf(w, "Error: %s", err)
+			return
+		}
+	}
+
+	return
 }
