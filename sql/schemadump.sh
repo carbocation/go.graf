0eecfef76f1ceafa2ec25919c289c6f32e777cc5
James Pirruccello <james@carbocation.com>
2013-04-18 15:45:53 -0400

    Updating schemadump.

--- sql/schemadump.sh
+++ sql/schemadump.sh
@@ -1,4 +1,4 @@
-pg_dump -h localhost -p 5432 -U projects -W --schema-only -n askbitcoin projects
+pg_dump -h localhost -p 5432 -U projectuser -W --schema-only -n askbitcoin projects
 
 # -h for host
 # -p for port
@@ -6,4 +6,5 @@ pg_dump -h localhost -p 5432 -U projects -W --schema-only -n askbitcoin projects
 # -W to prompt password
 # --schema-only to avoid dumping data
 # -n to specify the schema name
+# -C to produce create-database statements, not just create-schema statements
 # the final argument is the database name (a database can have many schemas; a schema can have many tables)
