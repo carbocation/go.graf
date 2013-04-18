5d72c7f6dcd5b18f968c7ee2aa1560a1a1b085d6
James Pirruccello <james@carbocation.com>
2013-04-18 15:37:27 -0400

    Annotated schemadump.

--- sql/schemadump.sh
+++ sql/schemadump.sh
@@ -1,1 +1,9 @@
-pg_dump -h localhost -U jpirruccello --schema-only -n public forumtest
+pg_dump -h localhost -p 5432 -U projects -W --schema-only -n askbitcoin projects
+
+# -h for host
+# -p for port
+# -U username
+# -W to prompt password
+# --schema-only to avoid dumping data
+# -n to specify the schema name
+# the final argument is the database name (a database can have many schemas; a schema can have many tables)
