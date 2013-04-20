b1bab91c4902b53fe566d6c819681e2722034aed
James Pirruccello <james@carbocation.com>
2013-04-20 18:11:39 -0400

    scp.sh now schleps the file over, kills the other processes, and starts the new process.

--- scp.sh
+++ scp.sh
@@ -1,1 +1,3 @@
-scp forum.linux james@carbocation.com:/data/bin/forum.linux
+#scp forum.linux james@carbocation.com:/data/bin/upload.forum.linux
+
+ssh -n -f james@carbocation.com "sh -c 'cd /data/bin/; killall forum.linux; mv upload.forum.linux forum.linux;  nohup ./forum.linux > /dev/null 2>&1 &'"
