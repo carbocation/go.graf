a3df8db4f06c1b9c53cd2b9a7b3aa7fa1054bcf6
James Pirruccello <james@carbocation.com>
2013-05-01 00:56:57 -0400

    Two big changes.
    First, there is a new exported ConfigFile struct that contains all
    user-configurable data. This is so that you can create projects that
    basically consist of a config file and import this project and set
    the config; then you have a branded project. It's more limited than
    you might initially think, but it adds little cost.
    
    Second, the template handlers now emit errors to StdErr upon
    failure, so gone are the days when a templating failure would leave
    you with a blank webpage and a blank console.

--- main.go
+++ main.go
@@ -14,27 +14,42 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type GlobalValues struct {
-	Site         string "Site name"
-	ContactEmail string "Webmaster email address"
+// A config file is an object that nests various 
+// public and other config structures
+type ConfigFile struct {
+	Public *ConfigPublic
+}
+
+type ConfigPublic struct {
+	Site         string //Site name
+	ContactEmail string //Webmaster email address
 }
 
 var (
+	// Exported so it can be overrided
+	Config *ConfigFile = &ConfigFile{
+		Public: &ConfigPublic{
+			Site:         "Ask Bitcoin",
+			ContactEmail: "james@carbocation.com",
+		},
+	}
+
 	db        *sql.DB     = initdb()                                                                                   //db maintains a pool of connections to our database of choice 
 	appsecret             = `75Oop7MSN88WstKJSTyu9ALiO0Nbeckv/4/eDLDJcpXn0Ny1H9PdpzXDqApie77tZ04GFsdHehmzcMkAqh16Dg==` //64 bit random string generated with `openssl rand -base64 64`
 	store                 = sessions.NewFilesystemStore("", []byte(appsecret))                                         //With an empty first argument, this will put session files in os.TempDir() (/tmp)
 	router    *mux.Router = mux.NewRouter()                                                                            //Dynamic content is managed by handlers pointed at by the router 
-	globals               = GlobalValues{
-		Site:         "Ask Bitcoin",
-		ContactEmail: "james@carbocation.com",
-	}
 )
 
-func init() {
+// For exporting
+func main() {
+	//Only if we're running this package as the main package do we need to configure the maxprocs here
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	//Call the main process
+	Main()
 }
 
-func main() {
+func Main() {
 	// Defer the close of the DB in the main function so we'll have a pool of connections maintained until the program exits
 	defer db.Close()
 
