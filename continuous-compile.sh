06553f50b5d26228ef5095ffa97e87e8b76801b4
James Pirruccello <james@carbocation.com>
2013-05-01 15:28:25 -0400

    continuous-config.sh now continuously checks for race conditions.

--- continuous-compile.sh
+++ continuous-compile.sh
@@ -20,5 +20,5 @@ fi
 # Build the binary, then
 # add in the static components with NRSC, then
 # launch the binary in the background
-go build -o /tmp/${PWD##*/}-main.osx *.go && ./nrsc-script /tmp/${PWD##*/}-main.osx "static templates" -q && /tmp/${PWD##*/}-main.osx &
+go build -race -o /tmp/${PWD##*/}-main.osx *.go && ./nrsc-script /tmp/${PWD##*/}-main.osx "static templates" -q && /tmp/${PWD##*/}-main.osx &
 
