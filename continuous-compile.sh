1831f0b0607c85cad71ef1d246da47b5425f4d1d
James Pirruccello <james@carbocation.com>
2013-05-21 03:04:05 -0700

    Enabling logging to disk.
    1. Added logrotate template script.
    
    2. Created environments (env_dev and env_prod). Dev will probably
    continue to send logs to stdout, while prod will want to log all.
    
    3. Environments are mutually exclusive: trying to compile with both
    left in should lead to compile-time error.
    
    4. Modified continuous-compile script to exclude env_prod files.

--- continuous-compile.sh
+++ continuous-compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Enable negative pattern matching !(prod*).go
+shopt -s extglob
+
 # This script keeps watch on the current project and compiles it continuously as you change files.
 # If there are multiple projects with the same final directory name (e.g., /proj/rad and /lib/monster/rad), 
 # this will kill any other similarly-named running projects' binaries, potentially leading to havoc.
@@ -7,6 +10,8 @@
 # To run, execute the following:
 # /usr/local/bin/fswatch ./ ./continuous-compile.sh
 
+#NOT the production environment
+NOTENV='./!(env_prod*).go'
 echo 'Re-compiling with these options: '"$@"
 
 #Count the number of times the compiled app is running
@@ -20,4 +25,4 @@ fi
 # Build the binary, then
 # add in the static components with NRSC, then
 # launch the binary in the background
-GOMAXPROCS=4 go build "$@" -o /tmp/${PWD##*/}-main.osx *.go && ./nrsc-script /tmp/${PWD##*/}-main.osx "static templates" -q && /tmp/${PWD##*/}-main.osx &
+GOMAXPROCS=4 go build "$@" -o /tmp/${PWD##*/}-main.osx ${NOTENV} && ./nrsc-script /tmp/${PWD##*/}-main.osx "static templates" -q && /tmp/${PWD##*/}-main.osx &
