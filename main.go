b29cf3c969433e843595f9448f7c72072b00aad6
James Pirruccello <james@carbocation.com>
2013-08-04 00:01:04 -0400

    Logging errors to the log file.

--- main.go
+++ main.go
@@ -11,16 +11,18 @@ import (
 )
 
 var (
-	Config    *ConfigFile
-	db        *sql.DB                                     //db maintains a pool of connections to our database of choice
-	store     *sessions.FilesystemStore                   //With an empty first argument, this will put session files in os.TempDir() (/tmp)
-	router    *mux.Router               = mux.NewRouter() //Dynamic content is managed by asksite.Handlers pointed at by the router
-	LogWriter *log.Logger
-	decoder   *schema.Decoder
+	Config         *ConfigFile
+	db             *sql.DB                   //db maintains a pool of connections to our database of choice
+	store          *sessions.FilesystemStore //With an empty first argument, this will put session files in os.TempDir() (/tmp)
+	router         *mux.Router               //Dynamic content is managed by asksite.Handlers pointed at by the router
+	LogWriter      *log.Logger
+	ErrorLogWriter *log.Logger
+	decoder        *schema.Decoder
 )
 
 func Initialize(cfg *ConfigFile, d *sql.DB, s *sessions.FilesystemStore, r *mux.Router, de *schema.Decoder) {
 	Config, db, store, router, decoder = cfg, d, s, r, de
 
 	LogWriter = log.New(Config.App.LogAccess, "", 0)
+	ErrorLogWriter = log.New(Config.App.LogError, "", 0)
 }
